3277212bdb57deb4ac3aa2b9f27fb683fce2a0d3
mobinkazemi <s.mobinkazemi@gmail.com>
2024-07-12 15:12:38 +0330

    router - feat - switches/neighbor routes defined

--- router/__init__.py
+++ router/__init__.py
@@ -1,6 +1,8 @@
 from fastapi import APIRouter
-from switches.router import router as switches_router
+from switches.routers.switches_router import router as switches_router
+from switches.routers.neighbors_router import router as neighbors_router
 
 routes = APIRouter()
 
 routes.include_router(switches_router, prefix="/switches", tags=["switches"])
+routes.include_router(neighbors_router, prefix="/switches/neighbor", tags=["switches"])
