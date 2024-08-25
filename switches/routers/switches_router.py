3608a15d807069bdf1c4f54a3a0d7f514ee416b7
mobinkazemi <s.mobinkazemi@gmail.com>
2024-08-25 14:08:15 +0330

    fix bug

--- switches/routers/switches_router.py
+++ switches/routers/switches_router.py
@@ -61,7 +61,7 @@ def checkConnectionStatus():
 
     for sw in allSwitches:
         result = check_ssh_connection(sw)
-        finalResult.append({"id": sw["id"], "result": not result})
+        finalResult.append({"id": sw["id"], "result": result})
 
     return {"data": finalResult, "message": "درخواست انجام شد"}
 
