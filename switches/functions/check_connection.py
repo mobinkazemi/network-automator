20fa78f36efe4c423ebc2da2186024ced457f21c
mobinkazemi <s.mobinkazemi@gmail.com>
2024-09-02 09:37:07 +0330

    switch - fix - check ssh connection function bug fixed

--- switches/functions/check_connection.py
+++ switches/functions/check_connection.py
@@ -5,9 +5,10 @@ from switches.model import Switch
 
 def check_ssh_connection(data: Switch) -> bool:
     try:
-        client = paramiko.Transport((data.ip, 22))
-        client.connect(username=data.username, password=data.password)
+        client = paramiko.Transport((data["ip"], 22))
+        client.connect(username=data["username"], password=data["password"])
         client.close()
         return True
     except Exception as e:
+        print(e)
         return False
