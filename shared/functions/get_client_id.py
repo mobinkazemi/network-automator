5741a94dbe8f699275b65aadaf12236e5243fca3
mobinkazemi <s.mobinkazemi@gmail.com>
2024-09-02 09:37:35 +0330

    shared - fix - get clinet id function bug fixed

--- shared/functions/get_client_id.py
+++ shared/functions/get_client_id.py
@@ -3,6 +3,6 @@ from fastapi import Request
 
 def getClientId(req: Request) -> str | None:
     try:
-        return req.headers.clientId
+        return req.headers["clientId"]
     except Exception as e:
         return None
